426ea3ae78cb94a9d071d138057de56e4540167d
BWsix <itisbw62@gmail.com>
2021-07-22 20:40:22 +0800

    refactor the realtime post udpate system

--- src/hooks/useListener.tsx
+++ src/hooks/useListener.tsx
@@ -1,72 +1,76 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import firebase from "firebase/app";
 
 import { PostProps } from "../pages/posts/Post";
 
+interface ActionProps {
+  type: "add" | "edit" | "delete";
+  postId: string;
+  time: Date;
+}
+
 export const useListener = (
   posts: PostProps[],
   setPosts: React.Dispatch<React.SetStateAction<PostProps[]>>
 ) => {
+  const [action, setAction] = useState<ActionProps>();
+
   useEffect(() => {
     return firebase
       .firestore()
       .collection("activities")
-      .onSnapshot((snap) => {
-        snap.docs.forEach((doc) => {
-          let actionType = doc.get("type");
-          let postId = doc.get("postId");
-
-          if (actionType === "add") {
-            if (posts[0].postId === postId) return;
-
-            firebase
-              .firestore()
-              .collection("posts")
-              .doc(postId)
-              .get()
-              .then((new_doc) => {
-                setPosts((prev) => [
-                  {
-                    ...(new_doc.data() as PostProps),
-                    postId: new_doc.id,
-                    createdAt: new_doc.get("createdAt").toDate(),
-                  },
-                  ...prev,
-                ]);
-              });
-          } else if (actionType === "delete") {
-            if (!posts.map((post) => post.postId).includes(postId)) return;
-
-            setPosts((prev) => prev.filter((post) => post.postId !== postId));
-          } else if (actionType === "edit") {
-            if (!posts.map((post) => post.postId).includes(postId)) return;
-
-            firebase
-              .firestore()
-              .collection("posts")
-              .doc(postId)
-              .get()
-              .then((editedPost) => {
-                if (
-                  posts.find(
-                    (post) => post.content === editedPost.get("content")
-                  )
-                )
-                  return;
-
-                setPosts((prev) =>
-                  prev.map((post) => {
-                    if (post.postId !== postId) return post;
-                    return {
-                      ...post,
-                      content: editedPost.get("content"),
-                    };
-                  })
-                );
-              });
-          }
-        });
+      .doc("posts")
+      .onSnapshot((doc) => {
+        setAction(doc.data() as ActionProps);
       });
-  }, [posts]);
+  }, []);
+
+  useEffect(() => {
+    if (!action || action.time || !posts) return;
+
+    if (action.type === "add") {
+      if (posts[0].postId === action.postId) return;
+
+      firebase
+        .firestore()
+        .collection("posts")
+        .doc(action.postId)
+        .get()
+        .then((new_doc) => {
+          setPosts((prev) => {
+            return [
+              {
+                ...(new_doc.data() as PostProps),
+                postId: new_doc.id,
+                createdAt: new_doc.get("createdAt").toDate(),
+              },
+              ...prev,
+            ];
+          });
+        });
+    } else if (action.type === "delete") {
+      setPosts(posts.filter((post) => post.postId !== action.postId));
+    } else if (action.type === "edit") {
+      if (!posts.map((post) => post.postId).includes(action.postId)) return;
+
+      firebase
+        .firestore()
+        .collection("posts")
+        .doc(action.postId)
+        .get()
+        .then((doc) => {
+          setPosts((prev) => {
+            return prev.map((post) => {
+              if (post.postId !== action.postId) return post;
+
+              return {
+                ...post,
+                content: doc.get("content"),
+              };
+            });
+          });
+        });
+    }
+  }, [action, posts]);
 };
