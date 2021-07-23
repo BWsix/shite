0782b055edc3d989c33a9b7671083345c79bd81f
BWsix <itisbw62@gmail.com>
2021-07-23 12:32:57 +0800

    feat: navbar auto hide well... thank you SMAKSS from stackoverflow https://stackoverflow.com/questions/62497110/detect-scroll-direction-in-react-js#62497293

---
+++ src/hooks/useScroll.tsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+
+export const useScroll = () => {
+  const [scrollDir, setScrollDir] = useState<"up" | "down">("up");
+
+  useEffect(() => {
+    const threshold = 0;
+    let lastScrollY = window.pageYOffset;
+    let ticking = false;
+
+    const updateScrollDir = () => {
+      const scrollY = window.pageYOffset;
+
+      if (Math.abs(scrollY - lastScrollY) < threshold) {
+        ticking = false;
+        return;
+      }
+      setScrollDir(scrollY > lastScrollY ? "up" : "down");
+      lastScrollY = scrollY > 0 ? scrollY : 0;
+      ticking = false;
+    };
+
+    const onScroll = () => {
+      if (!ticking) {
+        window.requestAnimationFrame(updateScrollDir);
+        ticking = true;
+      }
+    };
+
+    window.addEventListener("scroll", onScroll);
+    console.log(scrollDir);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, [scrollDir]);
+  return scrollDir === "down";
+};
--- src/routes/navbar/Navbar.tsx
+++ src/routes/navbar/Navbar.tsx
@@ -5,6 +5,7 @@ import firebase from "firebase/app";
 
 import { Menu } from "./Menu";
 import { UserIcon } from "../../components/hub";
+import { useScroll } from "../../hooks/useScroll";
 
 import "./Navbar.css";
 import "../../styles/button.css";
@@ -15,12 +16,14 @@ interface NavbarProps {
 
 export const Navbar: React.FC<NavbarProps> = ({ user }) => {
   const [toggle, setToggle] = useState(false);
+  const scrollDown = useScroll();
 
   return (
-    <div className="Nav-outer">
+    <div className={`Nav-outer ${scrollDown ? "" : "Nav-hide"}`}>
       <div className="Nav-inner">
         <Link to="/">
           <h1 className="Nav-h1">Shite.</h1>
+          {scrollDown}
         </Link>
 
         <div
